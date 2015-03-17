994d35e860b438d0e40ffd41d9788caadf1aa4a3
MKS-Student <mks.student@hackreactor.com>
2015-03-16 19:08:12 -0700

    Formatting correct.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,5 +1,7 @@
 // YOUR CODE HERE:
 
+var counter = 0;
+
 var initialize = function(){
 
 }
@@ -8,7 +10,7 @@ var getMessages = function(){
 	$.ajax({
 		url: 'https://api.parse.com/1/classes/chatterbox',
 		type: 'GET',
-		// data: JSON.stringify({key: 'value'}), 
+		data: JSON.stringify({key: 'value'}), 
 		contentType: 'application/json',
 		success: function (data) {
 		  console.log('chatterbox: New Messages Received!');
@@ -48,13 +50,24 @@ var createMessage = function (userName, message, room) {
 }
 
 var displayMessages = function(msg){
+	counter++;
 	for (var i = 0; i < msg.results.length; i++) {
 		var currentMessage = msg.results[i];
 		if (currentMessage.roomname === $('select').val()) {
-			var span = $('<span></span>').addClass('chat').text(JSON.stringify(currentMessage));
-			$('#main').append(span);
+			// create message div
+			var messageDiv = $('<div></div>').addClass('chat')
+			// create username div
+			var usernameDiv = $('<div></div>').addClass('username').text((currentMessage.username).toString() + ":");
+			// create text div
+			var textDiv = $('<div></div>').addClass('chat').text( (currentMessage.text).toString() );
+			// add text div to message div
+			$(messageDiv).append(usernameDiv);
+			// add message div to chat div
+			$(messageDiv).append(textDiv);
+			// add username div to message div
+			$('#chatDiv').prepend(messageDiv);
 		}
 	}
 }
 
-window.setInterval(getMessages, 1000);
+window.setInterval(getMessages, 2000);
