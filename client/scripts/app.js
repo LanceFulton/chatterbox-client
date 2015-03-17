c03092fb616c8d040b991c0a6f3fc0d259efbd8f
MKS-Student <mks.student@hackreactor.com>
2015-03-17 15:21:40 -0700

    Refactored to match specs.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,21 +1,23 @@
 // YOUR CODE HERE:
+var app = {};
 
 var counter = 0;
 
-var initialize = function(){
+app.init = function(){
+
 
 }
 
-var getMessages = function(){
+app.fetch = function(){
 	$.ajax({
 		url: 'https://api.parse.com/1/classes/chatterbox',
 		type: 'GET',
-		data: JSON.stringify({key: 'value'}), 
+		data: {"order": "-createdAt"},//, "where":'{"createdAt": {"$gt": "latestTime"} }' },
 		contentType: 'application/json',
 		success: function (data) {
 		  console.log('chatterbox: New Messages Received!');
+		  console.log(data);
 		  displayMessages(data);
-		  console.log(data)
 		},
 		error: function (data) {
 		  console.error('chatterbox: Failed to get new messages!');
@@ -23,13 +25,14 @@ var getMessages = function(){
 	})
 };
 
-var postMessage = function(message){
+app.send = function(message){
 	$.ajax({
 		url: 'https://api.parse.com/1/classes/chatterbox',
 		type: 'POST',
 		data: JSON.stringify(message),
 		contentType: 'application/json',
 		success: function (data) {
+			console.log('Send Data: ', data)
 		  console.log('chatterbox: Message sent');
 		},
 		error: function (data) {
@@ -38,36 +41,67 @@ var postMessage = function(message){
 	});
 };
 
-var createMessage = function (userName, message, room) {
+
+app.addMessage = function () {
+
+	var user = location.search.slice(10,location.search.length);
 
 	var message = {
-		username: userName,
-		text: message,
-		roomname: room,
+		username: user,
+		text: $('#input').val(),
+		roomname: $('#roomSelect').val(),
 	};
-
-	return message;
+	app.send(message);
 }
 
+app.clearMessages = function () {
+	//$('#chats');
+}
+
+app.addRoom = function(newRoom) {
+	var nRoom = $('<selector></selector>').text(newRoom);
+	$('#roomSelect').append(nRoom);
+}
+
+$(document).ready(function() {
+	var $userLink = location.search.slice(10,location.search.length);
+	$('#button').on( 'click', function(){
+		app.addMessage();
+		$('#input').val("");
+	} );
+	$($userLink).on('click', function () {
+		console.log('click')
+	});
+
+});
+
+var x = 0;
 var displayMessages = function(msg){
 	counter++;
+	//var latestTime = msg.results[0].createdAt;
 	for (var i = 0; i < msg.results.length; i++) {
 		var currentMessage = msg.results[i];
-		if (currentMessage.roomname === $('select').val()) {
+		if (currentMessage.roomname === $('roomSelect').val()) {
 			// create message div
 			var messageDiv = $('<div></div>').addClass('chat')
 			// create username div
-			var usernameDiv = $('<div></div>').addClass('username').text((currentMessage.username).toString() + ":");
+			var usernameDiv = $('<div></div>').addClass('username').text((currentMessage.username) + ":");
 			// create text div
-			var textDiv = $('<div></div>').addClass('chat').text( (currentMessage.text).toString() );
+			var textDiv = $('<div></div>').text( (currentMessage.text) );
 			// add text div to message div
 			$(messageDiv).append(usernameDiv);
 			// add message div to chat div
 			$(messageDiv).append(textDiv);
 			// add username div to message div
-			$('#chatDiv').prepend(messageDiv);
+			$('#chats').prepend(messageDiv);
+			var messageTime = currentMessage.createdAt;
 		}
 	}
 }
+window.setInterval(app.fetch, 2000);
 
-window.setInterval(getMessages, 2000);
+
+
+
+
+
